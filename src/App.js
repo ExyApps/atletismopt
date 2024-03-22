4bb7a10e4930f0ddc9a825524eb6be02049a7fbe
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2024-03-22 16:51:41 +0000

    Restarting; Start of main menu

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import { createTheme, ThemeProvider } from '@mui/material';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from './Pages/Home';
-import Backoffice from './Pages/Backoffice';
 import './App.css';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      'sans-serif',
-    ].join(','),
-  }
-});
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home'
+import React from 'react';
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="backoffice" element={<Backoffice />} />
-          <Route path="*" element={<h1>404</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,33 +1,55 @@
 import React from 'react';
 
-import Header from '../Components/Header/Header';
-import { Box } from '@mui/material';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import ImageCarrosel from '../Components/ImageCarousel/ImageCarousel';
 
 export default class Home extends React.Component {
-    render() {
-        return (
-            <Box>
-                <Header />
-                <Box
-                    sx={{
-                        height: "35px",
-                        width: "100%",
-                        backgroundColor: "#262626",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <p
-                        style={{
-                            color: "white",
-                            fontSize: "0.85rem",
-                        }}
-                    >
-                        <span className='gold-text'>Update:</span> Algum update que valha a pena ser mostrado
-                    </p>
-                </Box>
-            </Box>
-        )
-    }
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+	}
+
+	handleScroll(e) {
+		var homeContentsDiv = document.querySelector('.home-contents');
+		var homeContentsDivOffset = homeContentsDiv.offsetTop;
+		var currentScroll = window.scrollY;
+
+		var scrollPercent = Math.min(currentScroll / homeContentsDivOffset, 1);
+		var newTransparency = scrollPercent * 0.8 + 0.2;
+
+		document.querySelector('.navbar').style.background = `rgba(24, 24, 24, ${newTransparency})`;
+		// this.navbar.current.style.background = `rgba(0, 0, 0, ${newTransparency})`;
+	}
+
+	render() {
+		return (
+			<>
+				<Navbar
+					className="navbar fixed-top navbar-dark"
+				>
+					<Container>
+						<Navbar.Brand href="/">AtletismoPT</Navbar.Brand>
+						{/* <Nav className="me-0">
+							<Nav.Link href="/">Link 1</Nav.Link>
+							<Nav.Link href="/">Link 2</Nav.Link>
+							<Nav.Link href="/">Link 3</Nav.Link>
+						</Nav> */}
+					</Container>
+				</Navbar>
+
+				<ImageCarrosel />
+
+				<div className="home-contents">
+
+				</div>
+			</>
+		);
+	}
 }
