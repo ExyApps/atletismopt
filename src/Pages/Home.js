b807e5c229103be68934f7cda5962df6429f517b
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2024-03-24 19:27:12 +0000

    Hamburguer animation

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import ImageCarrosel from '../Components/ImageCarousel/ImageCarousel';
 
 export default class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			menuOpen: false
+		};
+	}
+
 	componentDidMount() {
 		window.addEventListener('scroll', this.handleScroll);
 	}
@@ -21,23 +25,31 @@ export default class Home extends React.Component {
 		var newTransparency = scrollPercent * 0.8 + 0.2;
 
 		document.querySelector('.navbar').style.background = `rgba(24, 24, 24, ${newTransparency})`;
-		// this.navbar.current.style.background = `rgba(0, 0, 0, ${newTransparency})`;
+	}
+
+	handleHamburgerClick() {
+		if (this.state.menuOpen) {
+			document.querySelector('.menu-btn').classList.remove('open');
+			this.setState({ menuOpen: false });
+		} else {
+			document.querySelector('.menu-btn').classList.add('open');
+			this.setState({ menuOpen: true });
+		}
 	}
 
 	render() {
 		return (
 			<>
 				<Navbar
-					className="navbar fixed-top navbar-dark"
+					className="navbar fixed-top"
 				>
-					<Container>
-						<Navbar.Brand href="/">AtletismoPT</Navbar.Brand>
-						{/* <Nav className="me-0">
-							<Nav.Link href="/">Link 1</Nav.Link>
-							<Nav.Link href="/">Link 2</Nav.Link>
-							<Nav.Link href="/">Link 3</Nav.Link>
-						</Nav> */}
-					</Container>
+					<div
+						className="menu-btn"
+						onClick={() => this.handleHamburgerClick()}
+					>
+						<div className="menu-btn__burger"></div>
+					</div>
+					<Navbar.Brand href="/">AtletismoPT</Navbar.Brand>
 				</Navbar>
 
 				<ImageCarrosel />
