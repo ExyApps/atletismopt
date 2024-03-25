e2a0184fd2ab5b6f0aaacdfe05a692436fc62ce2
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2024-03-25 16:41:37 +0000

    Sidemenu for phones

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+export default class NavbarComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			menuOpen: true,
+			transparencyValue: 0.2
+		};
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+	}
+
+	handleScroll(e) {
+		var homeContentsDiv = document.querySelector('.home-contents');
+		var homeContentsDivOffset = homeContentsDiv.offsetTop;
+		var currentScroll = window.scrollY;
+
+		var scrollPercent = Math.min(currentScroll / homeContentsDivOffset, 1);
+		var newTransparency = scrollPercent * 0.8 + 0.2;
+
+		document.querySelector('.navbar').style.background = `rgba(24, 24, 24, ${newTransparency})`;
+		this.setState({ transparencyValue: newTransparency });
+	}
+
+	handleHamburgerClick() {
+		if (this.state.menuOpen) {
+			document.querySelector('.menu-btn').classList.remove('open');
+			document.querySelector('.nav-links-sidebar').classList.remove('open');
+			document.querySelector('.navbar').style.background = `rgba(24, 24, 24, ${this.state.transparencyValue})`;
+			this.setState({ menuOpen: false });
+		} else {
+			document.querySelector('.menu-btn').classList.add('open');
+			document.querySelector('.nav-links-sidebar').classList.add('open');
+			document.querySelector('.navbar').style.background = `rgba(24, 24, 24, 1)`;
+			this.setState({ menuOpen: true });
+		}
+	}
+
+	render() {
+		return (
+			<>
+				<Navbar
+					className="navbar fixed-top"
+				>
+					<Navbar.Brand href="/">AtletismoPT</Navbar.Brand>
+
+					<Nav className="me-auto nav-links-container">
+						<Nav.Link className="pc-nav-link" href="/atletismopt/competicoes">Competições</Nav.Link>
+						<Nav.Link className="pc-nav-link" href="/atletismopt/perfis">Perfis</Nav.Link>
+						<Nav.Link className="pc-nav-link" href="/atletismopt/rankings">Rankings</Nav.Link>
+					</Nav>
+
+					<Nav.Link className="pc-nav-link" href="/atletismopt/sobre">Sobre</Nav.Link>
+
+					<div
+						className="menu-btn open"
+						onClick={() => this.handleHamburgerClick()}
+					>
+						<div className="menu-btn__burger"></div>
+					</div>
+				</Navbar>
+
+				<div className="nav-links-sidebar open">
+					<Nav.Link className="sidebar-nav-link" href="/atletismopt/competicoes">Competições</Nav.Link>
+					<Nav.Link className="sidebar-nav-link" href="/atletismopt/perfis">Perfis</Nav.Link>
+					<Nav.Link className="sidebar-nav-link" href="/atletismopt/rankings">Rankings</Nav.Link>
+					<Nav.Link className="sidebar-nav-link" href="/atletismopt/sobre">Sobre</Nav.Link>
+				</div>
+			</>
+		);
+	}
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import ImageCarrosel from '../Components/ImageCarousel/ImageCarousel';
+import Navbar from '../Components/Navbar/Navbar';
 
 export default class Home extends React.Component {
 	constructor(props) {
@@ -13,60 +14,13 @@ export default class Home extends React.Component {
 		};
 	}
 
-	componentDidMount() {
-		window.addEventListener('scroll', this.handleScroll);
-	}
-
-	handleScroll(e) {
-		var homeContentsDiv = document.querySelector('.home-contents');
-		var homeContentsDivOffset = homeContentsDiv.offsetTop;
-		var currentScroll = window.scrollY;
-
-		var scrollPercent = Math.min(currentScroll / homeContentsDivOffset, 1);
-		var newTransparency = scrollPercent * 0.8 + 0.2;
-
-		document.querySelector('.navbar').style.background = `rgba(24, 24, 24, ${newTransparency})`;
-	}
-
-	handleHamburgerClick() {
-		if (this.state.menuOpen) {
-			document.querySelector('.menu-btn').classList.remove('open');
-			this.setState({ menuOpen: false });
-		} else {
-			document.querySelector('.menu-btn').classList.add('open');
-			this.setState({ menuOpen: true });
-		}
-	}
-
 	render() {
 		return (
 			<>
-				<Navbar
-					className="navbar fixed-top"
-				>
-					<Navbar.Brand href="/">AtletismoPT</Navbar.Brand>
-
-					<Nav className="me-auto nav-links-container">
-						<Nav.Link className="pc-nav-link" href="/atletismopt/competicoes">Competições</Nav.Link>
-						<Nav.Link className="pc-nav-link" href="/atletismopt/perfis">Perfis</Nav.Link>
-						<Nav.Link className="pc-nav-link" href="/atletismopt/rankings">Rankings</Nav.Link>
-					</Nav>
-
-					<Nav.Link className="pc-nav-link" href="/atletismopt/sobre">Sobre</Nav.Link>
-
-					<div
-						className="menu-btn"
-						onClick={() => this.handleHamburgerClick()}
-					>
-						<div className="menu-btn__burger"></div>
-					</div>
-				</Navbar>
-
+				<Navbar />
 				<ImageCarrosel />
 
-				<div className="home-contents">
-
-				</div>
+				<div className="home-contents"></div>
 			</>
 		);
 	}
