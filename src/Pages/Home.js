e9b1ed1eb40aa0b5e01207eb7d09ec32bf5d930d
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2024-04-02 15:13:34 +0100

    Separate navbar styles

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
 		return (
 			<div className="page">
 				{/* <Navbar /> */}
-				<ImageCarrosel />
+				{/* <ImageCarrosel /> */}
 
 				<div className="home-contents"></div>
 			</div>
