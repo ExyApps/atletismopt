3a3b69a32ca151de091d9e6888cb08df21cbcd3a
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2024-04-18 14:04:18 +0100

    Sorting dropdowns for mobile results

--- src/Components/Table/AthleteResultsTable.js
+++ src/Components/Table/AthleteResultsTable.js
@@ -5,6 +5,8 @@ import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
 import NativeSelect from '@mui/material/NativeSelect';
 import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
 
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
@@ -82,113 +84,222 @@ export default class AthleteResultsTable extends Table {
 		const initialRow = Math.min(this.state.page * this.state.rowsPerPage + 1, this.state.searchedRows.length);
 		const finalRow = Math.min(this.state.page * this.state.rowsPerPage + this.state.rowsPerPage, this.state.searchedRows.length);
 
+		const orderOptions = [
+			{ name: 'Data', value: 'date' },
+			{ name: 'Evento', value: 'event_name' },
+			{ name: 'Competição', value: 'competition_name' },
+			{ name: 'Local', value: 'competition_location' },
+			{ name: 'Resultado', value: 'result' },
+			{ name: 'Vento', value: 'wind' }
+		]
+
+		const orderingOptions = [
+			{ name: 'Ascendente', value: 'asc' },
+			{ name: 'Decrescente', value: 'desc' }
+		]
+
 		return (
-			<table className="content-table">
-				<thead>
-					<tr>
-						<TableHeader
-							parent={this}
-							field=''
-							name='Resultados'
-							sortable={false}
-						/>
-					</tr>
-				</thead>
-				<tbody>
-					{
-						rows.length === 0
-							? <tr>
-								<td>
-									<div className="empty-table">
-										<span>Não foram encontrados resultados.</span>
-									</div>
-								</td>
-							</tr>
-							: rows.map((row, id) => (
-								<tr key={row.event_id} className="clickable-row" onClick={(event) => this.handleClick(event, row.event_id)}>
-									<Box sx={{
-										display: 'flex',
-										flexDirection: 'column',
-										alignItems: 'center',
-										justifyContent: 'center',
-										width: '100%',
-									}}>
-										<span className="mobile-span"><b>
-											{row.event_name} - {row.result}
-											{
-												row.wind === null || row.wind === undefined
-													? ''
-													: ` (${row.wind}v)`
-											}
-										</b></span>
-										<span className="mobile-span" style={{ fontSize: '13px' }}>{row.competition_name}</span>
-										<span className="mobile-span" style={{ fontSize: '13px' }}>{row.competition_location}</span>
-										<span className="mobile-span" style={{ fontSize: '14px' }}>{row.date}</span>
-									</Box>
+			<>
+				<span className='small-heading'>
+					Ordenar
+				</span>
+
+				<Box className='ordering-forms'>
+					<FormControl
+						size="small"
+						className='ordering-form'
+					>
+						<InputLabel
+							id={`order-sec-select-label`}
+							shrink={true}
+						>
+							Ordenar por
+						</InputLabel>
+
+						<NativeSelect
+							notched={true}
+							sx={{
+								minWidth: `120px`
+							}}
+							input={<OutlinedInput />}
+							id={`order-sec-select`}
+							defaultValue={this.state.orderBy}
+							value={this.state.orderBy}
+							label="Ordenar por"
+							onChange={(e) => {
+								this.setState(
+									{ "orderBy": e.target.value, page: 0 },
+									() => this.updateSearchedRows()
+								);
+							}}
+						>
+							{
+								orderOptions.map((option) => {
+									return <option
+										key={option.name}
+										value={option.value}
+									>
+										{option.name}
+									</option>
+								})
+							}
+						</NativeSelect>
+					</FormControl>
+
+					<FormControl
+						size="small"
+						className='ordering-form'
+					>
+						<InputLabel
+							id={`ordering-sec-select-label`}
+							shrink={true}
+						>
+							Direção
+						</InputLabel>
+
+						<NativeSelect
+							notched={true}
+							sx={{
+								minWidth: `120px`
+							}}
+							input={<OutlinedInput />}
+							id={`ordering-sec-select`}
+							defaultValue={this.state.order}
+							value={this.state.order}
+							label="Direção"
+							onChange={(e) => {
+								this.setState(
+									{ "order": e.target.value, page: 0 },
+									() => this.updateSearchedRows()
+								);
+							}}
+						>
+							{
+								orderingOptions.map((option) => {
+									return <option
+										key={option.name}
+										value={option.value}
+									>
+										{option.name}
+									</option>
+								})
+							}
+						</NativeSelect>
+					</FormControl>
+				</Box>
+
+				<table className="content-table">
+					<thead>
+						<tr>
+							<TableHeader
+								parent={this}
+								field=''
+								name='Resultados'
+								sortable={false}
+							/>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							rows.length === 0
+								? <tr>
+									<td>
+										<div className="empty-table">
+											<span>Não foram encontrados resultados.</span>
+										</div>
+									</td>
 								</tr>
-							))
-					}
-				</tbody>
-				<tfoot>
-					<tr>
-						<td>
-							<div className="foot-table">
-								<span>Linhas por página:</span>
+								: rows.map((row, id) => (
+									<tr
+										key={row.event_id}
+										className="clickable-row"
+										onClick={(event) => this.handleClick(event, row.event_id)}
+									>
+										<Box sx={{
+											m: '4px 0',
+											display: 'flex',
+											flexDirection: 'column',
+											alignItems: 'center',
+											justifyContent: 'center',
+											width: '100%',
+										}}>
+											<span className="mobile-span"><b>
+												{row.event_name} - {row.result}
+												{
+													row.wind === null || row.wind === undefined
+														? ''
+														: ` (${row.wind}v)`
+												}
+											</b></span>
+											<span className="mobile-span" style={{ fontSize: '13px' }}>{row.competition_name}</span>
+											<span className="mobile-span" style={{ fontSize: '13px' }}>{row.competition_location}</span>
+											<span className="mobile-span" style={{ fontSize: '14px' }}>{row.date}</span>
+										</Box>
+									</tr>
+								))
+						}
+					</tbody>
+					<tfoot>
+						<tr>
+							<td>
+								<div className="foot-table">
+									<span>Linhas por página:</span>
 
-								<FormControl
-									sx={{
-										marginLeft: '2rem',
-										marginRight: '2rem',
-									}}
-								>
-									<NativeSelect
-										className='foot-select'
-										size="small"
+									<FormControl
 										sx={{
-											color: "#fff",
-											'& .MuiSvgIcon-root': {
-												color: 'white'
-											}
+											marginLeft: '2rem',
+											marginRight: '2rem',
 										}}
-										disableUnderline
-										value={this.state.rowsPerPage}
-										inputProps={{
-											name: 'rowsPerPage',
-											id: 'uncontrolled-native',
-										}}
-										onChange={this.handleChangeRowsPerPage}
 									>
-										<option value={5}>5</option>
-										<option value={10}>10</option>
-										<option value={20}>20</option>
-									</NativeSelect>
-								</FormControl>
+										<NativeSelect
+											className='foot-select'
+											size="small"
+											sx={{
+												color: "#fff",
+												'& .MuiSvgIcon-root': {
+													color: 'white'
+												}
+											}}
+											disableUnderline
+											value={this.state.rowsPerPage}
+											inputProps={{
+												name: 'rowsPerPage',
+												id: 'uncontrolled-native',
+											}}
+											onChange={this.handleChangeRowsPerPage}
+										>
+											<option value={5}>5</option>
+											<option value={10}>10</option>
+											<option value={20}>20</option>
+										</NativeSelect>
+									</FormControl>
 
-								<span>
-									{`${initialRow}-${finalRow} de ${this.state.searchedRows.length}`}
-								</span>
+									<span>
+										{`${initialRow}-${finalRow} de ${this.state.searchedRows.length}`}
+									</span>
 
-								<Box>
-									<IconButton
-										sx={{ m: 0, p: 0, marginLeft: '2rem', color: "#fff" }}
-										onClick={(event) => this.handleChangePage(event, this.state.page - 1)}
-										disabled={this.state.page === 0}
-									>
-										<KeyboardArrowLeftIcon />
-									</IconButton>
-									<IconButton
-										sx={{ m: 0, p: 0, color: "#fff" }}
-										onClick={(event) => this.handleChangePage(event, this.state.page + 1)}
-										disabled={this.state.page >= Math.ceil(this.state.searchedRows.length / this.state.rowsPerPage) - 1}
-									>
-										<KeyboardArrowRightIcon />
-									</IconButton>
-								</Box>
-							</div>
-						</td>
-					</tr>
-				</tfoot>
-			</table>
+									<Box>
+										<IconButton
+											sx={{ m: 0, p: 0, marginLeft: '2rem', color: "#fff" }}
+											onClick={(event) => this.handleChangePage(event, this.state.page - 1)}
+											disabled={this.state.page === 0}
+										>
+											<KeyboardArrowLeftIcon />
+										</IconButton>
+										<IconButton
+											sx={{ m: 0, p: 0, color: "#fff" }}
+											onClick={(event) => this.handleChangePage(event, this.state.page + 1)}
+											disabled={this.state.page >= Math.ceil(this.state.searchedRows.length / this.state.rowsPerPage) - 1}
+										>
+											<KeyboardArrowRightIcon />
+										</IconButton>
+									</Box>
+								</div>
+							</td>
+						</tr>
+					</tfoot>
+				</table>
+			</>
 		)
 	}
 
